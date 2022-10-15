7cb9793da2588286bbceaca0d55e7bd9011ce555
wargs91 <benjamin.warger@gmail.com>
2022-10-15 05:15:31 -0700

    WebFrontEnd Built with Port and IP Display

---
+++ WebServerApp/WebClient/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Ubiquity Network";
+
+            RestClient restClient = new RestClient("http//localhost:49901/");
+            RestRequest restRequest = new RestRequest("api/UserRegistries", Method.Get);
+            RestResponse restResponse = restClient.Get(restRequest);
+
+            List<UserRegistry> userRegistries = JsonConvert.DeserializeObject<List<UserRegistry>>(restResponse.Content);
+
+            return View(userRegistries);
+        }
+    }
+}
