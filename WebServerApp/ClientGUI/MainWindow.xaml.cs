08f0cc6d642cfac53400209c7eff6d087501c801
wargs91 <benjamin.warger@gmail.com>
2022-10-15 02:16:00 -0700

    Browseer for files button added and debugged

---
+++ WebServerApp/ClientGUI/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ClientGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            bool? response = openFileDialog.ShowDialog();
+
+            if(response == true)
+            {
+                string filepath = openFileDialog.FileName;
+
+                MessageBox.Show(filepath);
+            }
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        { 
+        
+        }
+
+        private void ShowJobsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NetworkStatusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+    
